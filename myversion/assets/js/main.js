6c8db4184e5ed94738e2874a833a6fe7c78cde19
MO-Shourov <74562895+MO-Shourov@users.noreply.github.com>
2021-03-23 09:11:49 -0400

    Add files via upload

--- myversion/assets/js/main.js
+++ myversion/assets/js/main.js
@@ -1,54 +1,84 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId)=>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
 
-showMenu('nav-toggle','nav-menu')
+showMenu("nav-toggle", "nav-menu");
 
 /*===== REMOVE MENU =====*/
 
-const navLink = document.querySelectorAll('.nav__link'),
-    navMenu = document.getElementById('nav-menu')
+const navLink = document.querySelectorAll(".nav__link"),
+  navMenu = document.getElementById("nav-menu");
 
-function linkAction(){
-    navMenu.classList.remove('show')
+function linkAction() {
+  navMenu.classList.remove("show");
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
-function scrollActive (){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current=>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.OffsetTop - 50
-        sectionId = current.getAttribute('id')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.OffsetTop - 50;
+    sectionId = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-        document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-        
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
 }
 
-/*===== CHANGE COLOR HEADER =====*/ 
+/*===== CHANGE COLOR HEADER =====*/
 
-window.onscroll = ()=>{
-    const nav = document .getElementById('header')
-    if(this.scrollY >=200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+window.onscroll = () => {
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 2) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+};
+// Carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
